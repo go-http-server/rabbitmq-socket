44238557e484d9cadfe367b808cc0d5eab02d06f
phamnam2003 <namphamhai7@gmail.com>
2024-12-19 16:02:28 +0700

    feat(log): use zerolog serve log Stdout

--- cmd/server.go
+++ cmd/server.go
@@ -1,16 +1,14 @@
 package main
 
 import (
-	"fmt"
-	"log"
-
 	"github.com/go-http-server/rabbitmq-socket/utils"
+	"github.com/rs/zerolog/log"
 )
 
 func main() {
 	envVars, err := utils.LoadEnvironmentVariables("./")
 	if err != nil {
-		log.Fatal("cannot load environment variables: ", err)
+		log.Fatal().Err(err).Msg("cannot load environment variables")
 	}
-	fmt.Println(envVars.HTTP_SERVER_ADDRESS)
+	log.Info().Str("HTTP_SERVER_ADDRESS", envVars.HTTP_SERVER_ADDRESS).Msg("loaded environment variables")
 }
