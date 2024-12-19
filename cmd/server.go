8709d6a65a2be30b57b3f594368788f365e156db
phamnam2003 <namphamhai7@gmail.com>
2024-12-19 15:43:52 +0700

    feat(config): read success file .env -> get vars

--- cmd/server.go
+++ cmd/server.go
@@ -1,4 +1,16 @@
 package main
 
+import (
+	"fmt"
+	"log"
+
+	"github.com/go-http-server/rabbitmq-socket/utils"
+)
+
 func main() {
+	envVars, err := utils.LoadEnvironmentVariables("./")
+	if err != nil {
+		log.Fatal("cannot load environment variables: ", err)
+	}
+	fmt.Println(envVars.HTTP_SERVER_ADDRESS)
 }
