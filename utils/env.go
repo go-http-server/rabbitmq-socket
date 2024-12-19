1a153c83aeb326b20f6624cdd3390420a833a4e3
phamnam2003 <namphamhai7@gmail.com>
2024-12-19 15:34:06 +0700

    feat(config): read config from .env file with viper lib

---
+++ utils/env.go
@@ -0,0 +1,24 @@
+package utils
+
+import "github.com/spf13/viper"
+
+type EnvironmentVariables struct {
+	RABBITMQ_URL        string `mapstructure:"RABBITMQ_URL"` // connection string to rabbitmq, have host, port (or url), username, password, vhost (default vhost is '/')
+	HTTP_SERVER_ADDRESS string `mapstructure:"HTTP_SERVER_ADDRESS"`
+}
+
+func LoadEnvironmentVariables(pathname string) (vars EnvironmentVariables, err error) {
+	viper.AddConfigPath(pathname)
+	viper.SetConfigName(".env")
+	viper.SetConfigType("env")
+
+	viper.AutomaticEnv()
+
+	err = viper.ReadInConfig()
+	if err != nil {
+		return
+	}
+
+	err = viper.Unmarshal(&vars)
+	return
+}
